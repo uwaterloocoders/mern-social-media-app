c9c80be52477c108eaba00a04ac9295cf3fdd15a
Kushal Goel <64199835+kushalgoel786@users.noreply.github.com>
2023-02-16 19:14:03 -0500

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,4 +27,31 @@ export const getPost = async (req, res) => {
   }
 };
 
+export const createPost = async (req, res) => {
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
 export default router;
